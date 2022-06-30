4f0c623882713b2f3755d12a707b375a25502e5c
nasu <nasu@ekzm.co.jp>
2022-06-30 10:06:35 +0900

    user機能の実装

--- src/Application.php
+++ src/Application.php
@@ -16,6 +16,10 @@ declare(strict_types=1);
  */
 namespace App;
 
+use Authentication\AuthenticationService;
+use Authentication\AuthenticationServiceInterface;
+use Authentication\AuthenticationServiceProviderInterface;
+use Authentication\Middleware\AuthenticationMiddleware;
 use Cake\Core\Configure;
 use Cake\Core\ContainerInterface;
 use Cake\Datasource\FactoryLocator;
@@ -27,6 +31,8 @@ use Cake\Http\MiddlewareQueue;
 use Cake\ORM\Locator\TableLocator;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
+use Cake\Routing\Router;
+use Psr\Http\Message\ServerRequestInterface;
 
 /**
  * Application setup class.
@@ -34,7 +40,7 @@ use Cake\Routing\Middleware\RoutingMiddleware;
  * This defines the bootstrapping logic and middleware layers you
  * want to use in your application.
  */
-class Application extends BaseApplication
+class Application extends BaseApplication implements AuthenticationServiceProviderInterface
 {
     /**
      * Load all the application configuration and bootstrap logic.
@@ -66,6 +72,7 @@ class Application extends BaseApplication
         // Load more plugins here
         $this->addPlugin('AdminLTE');
         $this->addPlugin('MyAdminLTE');
+        $this->addPlugin('Authentication');
     }
 
     /**
@@ -103,7 +110,9 @@ class Application extends BaseApplication
             // https://book.cakephp.org/4/en/security/csrf.html#cross-site-request-forgery-csrf-middleware
             ->add(new CsrfProtectionMiddleware([
                 'httponly' => true,
-            ]));
+            ]))
+
+            ->add(new AuthenticationMiddleware($this));
 
         return $middlewareQueue;
     }
@@ -134,5 +143,35 @@ class Application extends BaseApplication
         $this->addPlugin('Migrations');
 
         // Load more plugins here
+    }
+
+    /**
+     * @param  \Psr\Http\Message\ServerRequestInterface  $request request
+     * @return \Authentication\AuthenticationServiceInterface
+     */
+    public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface
+    {
+        $service = new AuthenticationService([
+            'unauthenticatedRedirect' => Router::url('/admin/users/login'),
+            'queryParam' => 'redirect',
+                                             ]);
+        $fields = [
+            'username' => 'email',
+            'password' => 'password',
+        ];
+        $service->loadIdentifier('Authentication.Password', [
+            'fields' => $fields,
+        ]);
+
+        $service->loadAuthenticator('Authentication.Session', [
+            'identify' => true,
+            'fields' => ['username' => 'email'],
+        ]);
+        $service->loadAuthenticator('Authentication.Form', [
+            'fields' => $fields,
+            'loginUrl' => Router::url('/admin/users/login'),
+        ]);
+
+        return $service;
     }
 }
